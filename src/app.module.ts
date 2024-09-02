3f28812bbbd167b4abcf4dfebaeab1733989d39d
GuilhermeNallon <116029163+GuilhermeNallon@users.noreply.github.com>
2024-09-02 19:02:59 -0300

    feat: DatabaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './guards/auth.guard';
 import { JwtService } from '@nestjs/jwt';
 import { InfluencersModule } from './modules/influencers/influencers.module';
+import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
-  imports: [InfluencersModule],
+  imports: [DatabaseModule, InfluencersModule],
   controllers: [],
   providers: [
     JwtService,
